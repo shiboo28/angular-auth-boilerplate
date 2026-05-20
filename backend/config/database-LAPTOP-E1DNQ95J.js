e49ce7cfef24dcdfd7f9db1f099625ca9778bfa0
shiboo28 <jivolisao12345@gmail.com>
2026-05-20 17:08:57 +0800

    fix: remove all laptop files

--- backend/config/database-LAPTOP-E1DNQ95J.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Railway provides MYSQL_URL automatically when you link a MySQL service
-const sequelize = new Sequelize(process.env.MYSQL_URL || process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  logging: false
-});
-
-module.exports = sequelize;
--- src/app/admin/accounts/layout.component-LAPTOP-E1DNQ95J.ts
+++
@@ -1,3 +0,0 @@
-import { Component } from '@angular/core';
-@Component({ templateUrl: 'layout.component.html', standalone: false })
-export class LayoutComponent {}
