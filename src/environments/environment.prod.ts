a653a126758d870ce0a684bba992787728ca6127
shiboo28 <jivolisao12345@gmail.com>
2026-05-25 13:20:25 +0800

    fix: clean environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: 'https://node-mysql-api-production-46d3.up.railway.app'
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 3f54eb5 (fix: add https to apiUrl)
